b80ac83901dc68387061e153d37fc279449734a0
Daniel Mauricio B <mauricio.bueno@utp.edu.co>
2023-10-23 08:26:48 -0500

    Se crea el proyecto con las dependencias iniciales jpa,security,tools,web.lombok, se configura el propertie con la db.

---
+++ src/main/java/com/login/jwt/LoginSpringBoot3Application.java
@@ -0,0 +1,13 @@
+package com.login.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSpringBoot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSpringBoot3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/login/jwt/LoginSpringBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSpringBoot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
