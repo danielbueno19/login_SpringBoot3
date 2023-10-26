762163857659b6324643dfc8dd981230d3673a67
Daniel Mauricio B <mauricio.bueno@utp.edu.co>
2023-10-26 09:00:26 -0500

    se genera la logica para el apartado de login

--- src/main/java/com/login/jwt/Auth/AuthService.java
+++ src/main/java/com/login/jwt/Auth/AuthService.java
@@ -1,5 +1,9 @@
 package com.login.jwt.Auth;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.login.jwt.Jwt.JwtService;
@@ -15,16 +19,25 @@ public class AuthService {
 
     private final UserRepository userRepository;
     private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     public AuthResponse login(LoginRequest loginRequest) {
-        return null;
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        UserDetails user = userRepository.findByUsername(loginRequest.getUsername()).orElseThrow();
+
+        String token = jwtService.getToken(user);
+
+        return AuthResponse.builder()
+                        .token(token)
+                        .build();
     }
 
     public AuthResponse register(RegisterRequest registerRequest) {
 
         User user = User.builder()
                         .username(registerRequest.getUsername())
-                        .password(registerRequest.getPassword())
+                        .password(passwordEncoder.encode(registerRequest.getPassword()))
                         .firstname(registerRequest.getFirstname())
                         .lastname(registerRequest.getLastname())
                         .contry(registerRequest.getCountry())
